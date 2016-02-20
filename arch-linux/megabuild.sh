484906108b6b2367e4e505725c09831473ebaf1b
David Grayson <davidegrayson@gmail.com>
2016-02-19 19:51:35 -0800

    megabuild.sh: added more stuff

--- arch-linux/megabuild.sh
+++ arch-linux/megabuild.sh
@@ -1,3 +1,4 @@
+# Toolchain:
 # 1) lazy
 # 2) midipix-binutils
 # 3) midipix-gcc-stage1
@@ -6,3 +7,12 @@
 # 6) midipix-gcc-stage2
 # 7) midipix-musl
 # 8) midipix-gcc
+
+# Midipix tools/libraries:
+# 9) midipix-psxtypes
+# 10) midipix-dalist
+# 11) midipix-pemagine
+# 12) midipix-ntapi
+# 13) midipix-ntcon
+# 14) midipix-ntctty
+# 15) midipix-psxscl
